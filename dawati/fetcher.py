694f2b0d2819a6190df2dc6c87af592f828cbe27
Damien Lespiau <damien.lespiau@intel.com>
2012-10-12 12:25:31 +0100

    fetcher: Support Basic HTTP Auth to talk to OBS

--- dawati/fetcher.py
+++ dawati/fetcher.py
@@ -1,7 +1,11 @@
 # coding=utf-8
 
+import string
+
 import urllib2
 from message import Message
+
+from dawati import config
 
 class Fetcher(object):
     _instance = None
@@ -9,6 +13,25 @@ class Fetcher(object):
         if not cls._instance:
             cls._instance = super(Fetcher, cls).__new__(cls, *args, **kwargs)
         return cls._instance
+
+    def __init__(self):
+        try:
+            user = config.get('user')
+            password = config.get('password')
+
+            # extract the top level URL (skip http[s]:// and search for '/'))
+            trunk = config.get('obs_repo_trunk')
+            end = string.find(trunk, '/', 8)
+            top_level_url = trunk[:end]
+
+            manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
+            manager.add_password(None, top_level_url, user, password)
+            handler = urllib2.HTTPBasicAuthHandler(manager)
+            opener = urllib2.build_opener(handler)
+            urllib2.install_opener(opener)
+
+        except KeyError:
+            pass
 
     def get(self, url):
         m = Message()
